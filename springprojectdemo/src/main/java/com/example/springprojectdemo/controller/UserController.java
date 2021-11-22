f63539ba4877577c0dc857139f22c6cfe41cea67
alibek228k <80194360+alibek228k@users.noreply.github.com>
2021-11-22 14:25:11 +0600

    Add files via upload

---
+++ springprojectdemo/src/main/java/com/example/springprojectdemo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.springprojectdemo.controller;
+
+import com.example.springprojectdemo.model.User;
+import com.example.springprojectdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "userListForUsers";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "/user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
